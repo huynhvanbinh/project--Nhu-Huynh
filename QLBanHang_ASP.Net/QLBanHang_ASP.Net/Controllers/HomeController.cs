886755767b929d7b2a9e328a653302baf7eae893
huynhvanbinh <0306181203@caothang.edu.vn>
2021-10-05 15:42:29 +0700

    van binh dat hang lan 2

--- QLBanHang_ASP.Net/QLBanHang_ASP.Net/Controllers/HomeController.cs
+++ QLBanHang_ASP.Net/QLBanHang_ASP.Net/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using QLBanHang_ASP.Net.Models;
 
 namespace QLBanHang_ASP.Net.Controllers
@@ -11,9 +12,10 @@ namespace QLBanHang_ASP.Net.Controllers
     {
         QLBanHangEntities db = new QLBanHangEntities();
 
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
-            return View();
+            var lstSP = db.SanPhams;
+            return View(lstSP);
         }
         [HttpGet]
         public ActionResult MenuPartial()
--- QLBanHang_ASP.Net/QLBanHang_ASP.Net/Controllers/SanPhamController.cs
+++ QLBanHang_ASP.Net/QLBanHang_ASP.Net/Controllers/SanPhamController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PagedList;
+using QLBanHang_ASP.Net.Models;
 namespace QLBanHang_ASP.Net.Controllers
 {
     public class SanPhamController : Controller
     {
         // GET: SanPham
-        public ActionResult SanPham()
+        QLBanHangEntities db = new QLBanHangEntities();
+        [HttpGet]
+        public ActionResult SanPham(int? page, string Ma)
         {
-            return View();
+            var lstSanPhamLMT = db.SanPhams.Where(n=>n.GioTinh==Ma);
+            int PageSize = 15;
+            int PageNumber = (page ?? 1);
+            ViewBag.ListSP = lstSanPhamLMT;
+            ViewBag.Ma = Ma;
+            return View(lstSanPhamLMT.OrderBy(n => n.MaSP).ToPagedList(PageNumber, PageSize));
+
+        }
+        [HttpPost]
+        public ActionResult SanPham(int? page, string Ma, FormCollection f)
+        {
+            var lstSanPhamLMT = db.SanPhams.Where(n => n.GioTinh == Ma);
+            int PageSize = 15;
+            int PageNumber = (page ?? 1);
+            ViewBag.ListSP = lstSanPhamLMT;
+            ViewBag.Ma = Ma;
+            return View(lstSanPhamLMT.OrderBy(n => n.MaSP).ToPagedList(PageNumber, PageSize));
+
+        }
+
+        public ActionResult MenuSanPham(int? page, string Ma)
+        {
+            var lstSanPhamLMT = db.SanPhams.Where(n => n.GioTinh == Ma);
+            return PartialView(lstSanPhamLMT);
         }
     }
 }
