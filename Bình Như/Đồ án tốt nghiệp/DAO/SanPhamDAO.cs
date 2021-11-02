18e32ff14061417472776ee1321b4d6556018f9c
huynhvanbinh <0306181203@caothang.edu.vn>
2021-11-02 20:37:49 +0700

    vanbinh

--- Bình Như/Đồ án tốt nghiệp/DAO/SanPhamDAO.cs
+++ Bình Như/Đồ án tốt nghiệp/DAO/SanPhamDAO.cs
@@ -76,50 +76,7 @@ namespace DAO
             }).Take(2).ToList();
             return Dssp;
         }
-        public List<SanPhamDTO> layDSSPHD(string masp, string masp1, string masp2, string masp3, string masp4, string masp5, string masp6,
-            string masp7, string masp8, string masp9, string masp10, string masp11, string masp12, string masp13, string masp14, string masp15,
-            string masp16, string masp17, string masp18, string masp19, string masp20, string macuahang)
-        {
-            List<SanPhamDTO> Dssp = new List<SanPhamDTO>();
-            Dssp = demos.SanPhams.Where(u => u.TrangThai == "1" &&u.MaSP != masp && u.MaSP != masp1 && u.MaSP != masp2 && u.MaSP != masp3
-            && u.MaSP != masp4 && u.MaSP != masp5 && u.MaSP != masp6 && u.MaSP != masp7 && u.MaSP != masp8 && u.MaSP != masp9 && u.MaSP != masp10 && u.MaSP != masp11
-            && u.MaSP != masp12 && u.MaSP != masp13 && u.MaSP != masp14 && u.MaSP != masp15 && u.MaSP != masp16 && u.MaSP != masp17 && u.MaSP != masp18
-            && u.MaSP != masp19 && u.MaSP != masp20 ).Select(u => new SanPhamDTO
-            {
-                MaSP = u.MaSP,
-                TenSP = u.TenSP,
-                DonGia = u.DonGia,
-                MaLoai = u.MaLoai,
-                GiaNhap = u.GiaNhap,
-                SoLuongTon = u.SoLuongTon,
-                MoTa = u.MoTa,
-                HinhAnh = u.HinhAnh,
-                TrangThai = u.TrangThai,
-            }).ToList();
-            return Dssp;
-        }
-        public List<SanPhamDTO> layDSSPKM(string masp, string masp1, string masp2, string masp3, string masp4, string masp5, string masp6,
-           string masp7, string masp8, string masp9, string masp10, string masp11, string masp12, string masp13, string masp14, string masp15,
-           string masp16, string masp17, string masp18, string masp19, string masp20)
-        {
-            List<SanPhamDTO> Dssp = new List<SanPhamDTO>();
-            Dssp = demos.SanPhams.Where(u => u.TrangThai == "1" && u.MaSP != masp && u.MaSP != masp1 && u.MaSP != masp2 && u.MaSP != masp3
-            && u.MaSP != masp4 && u.MaSP != masp5 && u.MaSP != masp6 && u.MaSP != masp7 && u.MaSP != masp8 && u.MaSP != masp9 && u.MaSP != masp10 && u.MaSP != masp11
-            && u.MaSP != masp12 && u.MaSP != masp13 && u.MaSP != masp14 && u.MaSP != masp15 && u.MaSP != masp16 && u.MaSP != masp17 && u.MaSP != masp18
-            && u.MaSP != masp19 && u.MaSP != masp20).Select(u => new SanPhamDTO
-            {
-                MaSP = u.MaSP,
-                TenSP = u.TenSP,
-                DonGia = u.DonGia,
-                MaLoai = u.MaLoai,
-                GiaNhap = u.GiaNhap,
-                SoLuongTon = u.SoLuongTon,
-                MoTa = u.MoTa,
-                HinhAnh = u.HinhAnh,
-                TrangThai = u.TrangThai,
-            }).ToList();
-            return Dssp;
-        }
+       
         public SanPhamDTO laySP(string makh)
         {
             SanPhamDTO sp = new SanPhamDTO();
